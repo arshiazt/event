abd2fc224467eda8e3a6e41aef0223baa50b6dcc
arshiazt <arshiazabeti@gmail.com>
2026-08-26 21:44:50 +0330

    add RegistrationAdmin for manage regitration in admin panel

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from registration.models import Registration
 
 # Register your models here.
+
+class RegistrationAdmin(admin.ModelAdmin):
+    
+    list_display = ('user','event','status')
+    readonly_fields = ('created_at','canceled_at')
+    search_fields = ('user',)
+    list_filter = ('user','status')
+
+admin.site.register(Registration,RegistrationAdmin)

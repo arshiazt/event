eb22cea61dd75c6c4ceea26f8f8ba4331aa6743d
arshiazt <arshiazabeti@gmail.com>
2026-08-26 17:24:33 +0330

    add registerview api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login,logout
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from accounts.serializers import RegisterSerializer,LoginSerializer
+from accounts.permissions import IsNotAuthenticated
 
 # Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [IsNotAuthenticated]
+
+    def post(self,request):
+
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request,user)
+
+        return Response({
+            'message':'Registration was successful',
+            'phone':user.phone,
+            'role':'organizer' if user.organizer else 'participant',
+        },
+        status =  status.HTTP_201_CREATED
+        )

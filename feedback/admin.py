d27fe47335a0e7adc241a195b598205ee2dd58a4
arshiazt <arshiazabeti@gmail.com>
2026-08-26 22:13:46 +0330

    create rating & comment admin for manage admin panel

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from feedback.models import *
 
 # Register your models here.
+
+class RatingAdmin(admin.ModelAdmin):
+
+    list_display = ('user','event')
+    fields = ('user','event','value')
+    search_fields =  ('event',)
+    readonly_fields = ('user','event','value')
+
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('user','event')
+    fields = ('user','event','text')
+    search_fields =  ('event',)
+    readonly_fields = ('user','event','text')
+
+admin.site.register(EventComment,CommentAdmin)
+admin.site.register(EventRating,RatingAdmin)

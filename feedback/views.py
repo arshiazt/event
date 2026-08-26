c42ff8dba5815341d18bbe0fcc5e1e4f4d0d9d32
arshiazt <arshiazabeti@gmail.com>
2026-08-26 23:27:10 +0330

    add commentcreate view

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.db.models import Avg
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
+from event.models import Event
+from feedback.models import EventComment, EventRating
+from feedback.serializers import *
 
 # Create your views here.
+
+class CommentCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentCreateSerializer
+    def post(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+
+        serializer = CommentCreateSerializer(
+            data=request.data,
+            context={
+                "request": request,
+                "event": event
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            {"detail": "Comment submitted successfully."},
+            status=status.HTTP_201_CREATED
+        )

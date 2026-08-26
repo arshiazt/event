10659cd688782664a17c0fc2815c104c9d33c1f5
arshiazt <arshiazabeti@gmail.com>
2026-08-26 22:02:05 +0330

    add EventRegistrationListForOrganizerView for regitration app

--- registration/views.py
+++ registration/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from registration.models import Registration
 from registration.serializers import *
+from event.models import Event
 
 # Create your views here.
 
@@ -42,3 +43,21 @@ class ParticipantRegistrationCancelView(UpdateAPIView):
             raise PermissionDenied("Only participants can cancel registrations.")
 
         return Registration.objects.filter(user=user)
+
+class EventRegistrationListForOrganizerView(ListAPIView):
+    serializer_class = EventRegistrationListForOrganizerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        event_id = self.kwargs.get("event_id")
+
+        if not user.organizer:
+            raise PermissionDenied("Only organizers can view registrations.")
+
+        try:
+            event = Event.objects.get(id=event_id, owner=user)
+        except Event.DoesNotExist:
+            raise PermissionDenied("You do not own this event.")
+
+        return Registration.objects.filter(event=event)

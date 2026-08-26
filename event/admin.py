6feeddab1f41a2f47a1e93b5ff9abd436e4bd8be
arshiazt <arshiazabeti@gmail.com>
2026-08-26 20:46:32 +0330

    update admin.py (event app) for manage admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from event.models import *
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    
+    list_display = ('title','owner','status','published_date')
+    search_fields = ('title','owner')
+    readonly_fields = ('owner','created_date','updated_date')
+    fields = ('title','owner','capacity','status','published_date','start_date','end_date')
+
+class AttributeAdmin(admin.ModelAdmin):
+
+    list_display = ('name','value_type',)
+    search_fields = ('name',)
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Attribute,AttributeAdmin)
+admin.site.register(EventAttributeValue)
